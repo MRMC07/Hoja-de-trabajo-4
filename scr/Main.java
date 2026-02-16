0f84c87b349325082ae778842951a602ce581119
Dulce Granados <dulcegranados.chun06@gmail.com>
2026-02-15 18:03:59 -0600

    creacion de stacks

---
+++ scr/Main.java
@@ -0,0 +1,21 @@
+package scr;
+
+import java.util.Scanner;
+
+public class Main {
+    static void main (String[] args) { 
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Bienvenido a su calculadora! En que formato desea trabajar?");
+        System.out.println("1. Pila");
+        System.out.println("2. Lista");
+        System.out.println("Escoja una opción:");
+
+        int opcion = scanner.nextInt();
+
+        
+
+    
+    }
+}
