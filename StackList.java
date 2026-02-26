4342a4c1bd829aaf89abf5d5e63c67ea0e68d80e
MRMC07 <maremaza07@gmail.com>
2026-02-25 23:56:30 -0600

    Arreglos de StackList

--- StackList.java
+++ StackList.java
@@ -1,4 +1,37 @@
 package scr;
-public class StackList {
-    
+
+public class StackList implements IStack {
+
+    private IList<Integer> list;
+
+    public StackList(IList<Integer> list) {
+        this.list = list;
+    }
+
+    @Override
+    public void push(int value) {
+        list.addFirst(value);
+    }
+
+    @Override
+    public int pop() {
+        Integer value = list.removeFirst();
+        return value == null ? -1 : value;
+    }
+
+    @Override
+    public int peek() {
+        Integer value = list.getFirst();
+        return value == null ? -1 : value;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
 }
