9d1dba1ac2c4b3d0f9833fa53e12cce62d431593
MRMC07 <maremaza07@gmail.com>
2026-02-25 23:53:18 -0600

    Arreglos main

--- Main.java
+++ Main.java
@@ -3,19 +3,24 @@ package scr;
 import java.util.Scanner;
 
 public class Main {
-    static void main (String[] args) { 
-        
+
+    public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Bienvenido a su calculadora! En que formato desea trabajar?");
+
+        System.out.println("Bienvenido a su calculadora!");
         System.out.println("1. Pila");
-        System.out.println("2. Lista");
         System.out.println("Escoja una opción:");
 
         int opcion = scanner.nextInt();
 
-        
+        if (opcion == 1) {
+            IStack stack = StackFactory.createStack(1, scanner);
+            System.out.println("Pila creada correctamente.");
+        } else {
+            System.out.println("Opción no válida.");
+        }
 
-    
+        scanner.close();
     }
 }
